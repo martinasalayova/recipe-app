e60169c402ceac653800eb97ba2f2411831a5ecc
martinasalayova <vachkovamartina@gmail.com>
2023-10-05 15:35:58 +0200

    Added basic file construction

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Veggie from "../components/Veggie";
+import Popular from "../components/Popular";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <Veggie />
+      <Popular />
+    </div>
+  );
+}
