7d2b82025b9215e9f763b138695f52e0912273f0
martinasalayova <vachkovamartina@gmail.com>
2023-11-04 14:14:55 +0100

    Added new component for Vegetarian category, added new page Cuisine, Added react-router-dom from my NavLinks

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Home from "./Home";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Cuisine from "./Cuisine";
 
 export default function Pages() {
   return (
-    <div className="Pages">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine" element={<Cuisine />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
