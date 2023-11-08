95ad686873b4c53580b783c1c9d433132a706f51
martinasalayova <vachkovamartina@gmail.com>
2023-11-08 21:13:38 +0100

    Added functionality behind Recipe page

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
+import { Link } from "react-router-dom";
 
 export default function Popular() {
   const [popular, setPopular] = useState([]);
@@ -40,9 +41,11 @@ export default function Popular() {
           return (
             <SplideSlide key={recipe.id}>
               <Card>
-                <p>{recipe.title}</p>
-                <img src={recipe.image} alt={recipe.title}></img>
-                <Gradient />
+                <Link to={"/recipe/" + recipe.id}>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title}></img>
+                  <Gradient />
+                </Link>
               </Card>
             </SplideSlide>
           );
--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -3,7 +3,42 @@ import styled from "styled-components";
 import { useParams } from "react-router-dom";
 
 function Recipe() {
-  return <div>Recipe</div>;
+  let params = useParams();
+  const [details, setDetails] = useState({});
+  const fetchDetails = async () => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
+    );
+    const detailData = await data.json();
+    setDetails(detailData);
+  };
+
+  useEffect(() => {
+    fetchDetails();
+  }, [params.name]);
+
+  return <div>{details.title}</div>;
 }
+
+const DetailWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+  .active {
+    background: linear-gradient(35deg, #494949, #313131);
+    color: white;
+  }
+
+  h2 {
+    margin-bottom: 2rem;
+  }
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+  ul {
+    margin-top: 2rem;
+  }
+`;
 
 export default Recipe;
