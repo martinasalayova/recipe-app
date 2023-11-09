f4e3edaba49ef39c06eafd050349cf0434e3bf82
martinasalayova <vachkovamartina@gmail.com>
2023-11-09 22:17:55 +0100

    Finished project

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -5,19 +5,55 @@ import { useParams } from "react-router-dom";
 function Recipe() {
   let params = useParams();
   const [details, setDetails] = useState({});
+  const [activeTab, setActiveTab] = useState("instructions");
   const fetchDetails = async () => {
     const data = await fetch(
       `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
     );
     const detailData = await data.json();
     setDetails(detailData);
+    console.log(detailData);
   };
 
   useEffect(() => {
     fetchDetails();
   }, [params.name]);
 
-  return <div>{details.title}</div>;
+  return (
+    <DetailWrapper>
+      <div>
+        <h2>{details.title}</h2>
+        <img src={details.image} alt={details.title} />
+      </div>
+      <Info>
+        <Button
+          className={activeTab === "instructions" ? "active" : ""}
+          onClick={() => setActiveTab("instructions")}
+        >
+          Instructions
+        </Button>
+        <Button
+          className={activeTab === "ingredients" ? "active" : ""}
+          onClick={() => setActiveTab("ingredients")}
+        >
+          Ingredients
+        </Button>
+        {activeTab === "instructions" && (
+          <div>
+            <h3 dangerouslySetInnerHTML={{ __html: details.summary }}></h3>
+            <h3 dangerouslySetInnerHTML={{ __html: details.instructions }}></h3>
+          </div>
+        )}
+        {activeTab === "ingredients" && (
+          <ul>
+            {details.extendedIngredients.map((ingredient) => (
+              <li key={ingredient.id}>{ingredient.original}</li>
+            ))}
+          </ul>
+        )}
+      </Info>
+    </DetailWrapper>
+  );
 }
 
 const DetailWrapper = styled.div`
@@ -39,6 +75,19 @@ const DetailWrapper = styled.div`
   ul {
     margin-top: 2rem;
   }
+`;
+
+const Button = styled.div`
+  padding: 1rem 2rem;
+  color: #313131;
+  background: white;
+  border: 2px solid black;
+  margin-right: 2rem;
+  font-weight: 600;
+`;
+
+const Info = styled.div`
+  mergin-left: 10rem;
 `;
 
 export default Recipe;
